ea0f01e56e97b6e52a317d7ca965aed556b16d3b
dahuggin <dahuggin1@gmail.com>
2017-04-16 15:25:44 -0500

    Sign in UI Custom Classes

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Daniel Huggins on 4/16/17.
+//  Copyright © 2017 Daniel Huggins. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ SocialApp/FancyField.swift
@@ -0,0 +1,28 @@
+//
+//  FancyField.swift
+//  SocialApp
+//
+//  Created by Daniel Huggins on 4/16/17.
+//  Copyright © 2017 Daniel Huggins. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
