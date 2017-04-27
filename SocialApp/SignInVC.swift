ea7c49631ccf1e561cb6dc53abbf851c9c78c273
dahuggin <dahuggin1@gmail.com>
2017-04-26 20:12:38 -0500

    Firebase hooked up

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -54,7 +54,8 @@ class SignInVC: UIViewController {
             } else {
                 print("DH: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -66,7 +67,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("DH: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -75,7 +77,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("DH: Successfully authenticated email with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -84,7 +87,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("DH: Sign in completed - \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
